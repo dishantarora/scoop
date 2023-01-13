5d30fc6f35bf77c8d0fdfdf8b226185c3119d564
Dishant Arora <dishantarora@outlook.com>
2023-01-13 13:43:15 +0530

    Deleted files and replaced with other implementations

--- linkedin-scrapper/page_object/locator.py
+++
@@ -1,6 +0,0 @@
-class Locator(object):
-
-    # Homepage search form
-    job_title_search_field = "//*[@id='JOBS']/section[1]/input"
-    location_search_field = "//*[@id='JOBS']/section[2]/input"
-    search_jobs_button = '//*[@id="main-content"]/section[1]/div/section/div[2]/button[2]'
