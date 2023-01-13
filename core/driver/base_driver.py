4aff4f8845196c7a970cfabe380cac4948b0dc46
Dishant Arora <dishantarora@outlook.com>
2023-01-13 13:40:16 +0530

    Created an abstract class for webdriver.

---
+++ core/driver/base_driver.py
@@ -0,0 +1,14 @@
+from abc import ABC
+
+class BaseDriver(ABC):
+
+    def __init__(self):
+        self._driver = None
+
+    def load_application(self, url: str) -> None:
+        self._driver.get(url)
+
+    def close_driver(self):
+        if self._driver is not None:
+            self._driver.quit()
+            self._driver = None
