bde43c029376ccd9b46d44aa3b2e30ff482c819f
Dishant Arora <dishantarora@outlook.com>
2023-01-13 13:40:43 +0530

    Create an concrete implementation of chrome webdriver.

---
+++ core/driver/chrome_driver.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from core.driver.base_driver import BaseDriver
+
+class ChromeDriver(BaseDriver):
+
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.__config = config
+        self.__setup()
+
+    def __setup(self):
+        self._driver = webdriver.Chrome(
+            self.__config.get_browser_value('chrome_driver_path'))
