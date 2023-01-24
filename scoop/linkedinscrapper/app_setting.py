834a77e0eecce67846f4c67acf553c3bddd379e0
Dishant Arora <dishantarora@outlook.com>
2023-01-24 14:24:22 +0530

    Created a local level setting file for linkedinscrapper.

---
+++ scoop/linkedinscrapper/app_setting.py
@@ -0,0 +1,16 @@
+from core.util.setting import Setting
+
+
+class AppSetting(Setting):
+
+    def __init__(self):
+        super().__init__('scoop\\linkedinscrapper\\app-settings.yml')
+        self._application_settings = self._get_setting_data['application-setting']
+
+    @property
+    def application_url(self):
+        return self._application_settings['application-url']
+
+    @property
+    def job_title(self):
+        return self._application_settings['job-title']
