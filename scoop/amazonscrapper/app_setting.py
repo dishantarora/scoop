56bb9392a2fdcc7c5e438cb425a45a0718dc35ba
Dishant Arora <dishantarora@outlook.com>
2023-01-24 14:22:55 +0530

    Implement setting on local level.

---
+++ scoop/amazonscrapper/app_setting.py
@@ -0,0 +1,12 @@
+from core.util.setting import Setting
+
+
+class AppSetting(Setting):
+
+    def __init__(self):
+        super().__init__('scoop\\amazon-scrapper\\app-settings.yml')
+        self._application_settings = self._get_setting_data['application-setting']
+
+    @property
+    def product_name(self):
+        return self._application_settings['product-name']
