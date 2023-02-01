bf573e3672f9881606b22448f55a1f39351ea410
Dishant Arora <dishantarora@outlook.com>
2023-02-01 09:37:21 +0530

    Modified local setting.yml path and search product key.

--- scoop/amazonscrapper/app_setting.py
+++ scoop/amazonscrapper/app_setting.py
@@ -4,7 +4,7 @@ from core.util.setting import Setting
 class AppSetting(Setting):
 
     def __init__(self):
-        super().__init__('scoop\\amazon-scrapper\\app-settings.yml')
+        super().__init__('scoop\\amazonscrapper\\settings.yml')
         self._application_settings = self._get_setting_data['application-setting']
 
     @property
@@ -12,5 +12,5 @@ class AppSetting(Setting):
         return self._application_settings['application-url']
     
     @property
-    def product_name(self):
-        return self._application_settings['product-name']
+    def search_product(self):
+        return self._application_settings['search-product']
