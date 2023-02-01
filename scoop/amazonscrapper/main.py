8497f894c383f4d793a00a6dbde7d731d58f74d1
Dishant Arora <dishantarora@outlook.com>
2023-02-01 09:40:08 +0530

    Modified amazon scrapper implementation.

--- scoop/amazonscrapper/main.py
+++ scoop/amazonscrapper/main.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from core.util.system_setting import SystemSetting
 from core.driver.driver_factory import DriverFactory
 from scoop.amazonscrapper.app_setting import AppSetting
+from scoop.amazonscrapper.locators import Locator
+
 
 def main():
 
@@ -16,36 +19,54 @@ def main():
     driver.get(app_setting.application_url)
 
     # Enter search keyword in search field and submit.
-    search_field = driver.find_element(By.XPATH, r'//*[@id="twotabsearchtextbox"]')
-    search_submit_button = driver.find_element(By.XPATH, r'//*[@id="nav-search-submit-button"]')
+    try:
+        search_field = driver.find_element(By.XPATH, Locator.SEARCH_FIELD)
+    except NoSuchElementException:
+        exit()
+
+    try:
+        search_button = driver.find_element(By.XPATH, Locator.SEARCH_BUTTON)
+    except NoSuchElementException:
+        exit()
+
     search_field.clear()
-    search_field.send_keys(app_setting.product_name)
-    search_submit_button.click()
+    search_field.send_keys(app_setting.search_product)
+    search_button.click()
 
     # Verifying search result page.
 
-    search_result_items = driver.find_elements(By.XPATH, '')
+    search_result_items = driver.find_elements(
+        By.XPATH, "//div[@class='a-section a-spacing-small a-spacing-top-small']")
 
     products = []
 
-    for item in search_result_items:        
+    for item in search_result_items:
         temp = {}
-        temp['product_heading'] = item.find_element(By.XPATH, '').text
-        temp['rating'] = item.find_element(By.XPATH, '').text
-        temp['customer_reviews'] = item.find_element(By.XPATH, '').text
-        temp['current_price'] = item.find_element(By.XPATH, '').text
-        temp['actual_price'] = item.find_element(By.XPATH, '').text
-        temp['amount_saved'] = item.find_element(By.XPATH, '').text
-        temp['amount_saved_percentage'] = item.find_element(By.XPATH, '').text
-        temp['is_no_cost_emi_available'] = item.find_element(By.XPATH, '').text
-        temp['free_delivery_by'] = item.find_element(By.XPATH, '').text
-        temp['is_prime'] = item.find_element(By.XPATH, '').text
+        product_heading = item.find_element(
+            By.XPATH,
+            "//div/h2/a/span[@class='a-size-medium a-color-base a-text-normal']")
+        if product_heading is not None:
+            temp['product_heading'] = product_heading.text
+        else:
+            temp['product_heading'] = 'Not Found'
+        # temp['rating'] = item.find_element(By.XPATH, '').text
+        # temp['customer_reviews'] = item.find_element(By.XPATH, '').text
+        # temp['current_price'] = item.find_element(By.XPATH, '').text
+        # temp['actual_price'] = item.find_element(By.XPATH, '').text
+        # temp['amount_saved'] = item.find_element(By.XPATH, '').text
+        # temp['amount_saved_percentage'] = item.find_element(By.XPATH, '').text
+        # temp['is_no_cost_emi_available'] = item.find_element(By.XPATH, '').text
+        # temp['free_delivery_by'] = item.find_element(By.XPATH, '').text
+        # temp['is_prime'] = item.find_element(By.XPATH, '').text
         products.append(temp)
 
+    print(len(products))
+
     for product in products:
-        print(product.prettify())
-    
+        print(product['product_heading'])
+
     driver.quit()
+
 
 if __name__ == '__main__':
     main()
